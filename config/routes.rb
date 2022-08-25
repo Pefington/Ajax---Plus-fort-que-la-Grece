338f4ba282fa38d5fd349109dc8cd829c3c56aab
Pierre-François Salmand <maabaa@gmail.com>
2022-08-25 15:59:47 +0700

    Generate email scaffold (cleaned up)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'home/index'
+  root 'email#index'
+  resources :emails
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
